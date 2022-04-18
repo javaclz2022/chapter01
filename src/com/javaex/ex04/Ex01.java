f6f95212405b47d8d7a74c6ad7937112878d6148
javaclz2022 <javaclz2022@gmail.com>
2022-04-18 17:17:59 +0900

    2022-04-18 반복문1차

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,14 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while(i<5) { //6<=5 -->F
+			System.out.println("I like java" + i );
+			i++ ;  // i=i+1;
+		}
+		
+	}
+}
